7bf62396a0918fcf43b809744241429bdc588772
Smokedgar <54284730+Smokedgar@users.noreply.github.com>
2019-10-06 00:08:03 +0300

    Add files via upload

---
+++ task1/Sentence.java
@@ -0,0 +1,29 @@
+package by.htp.hw4.task1;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sentence {
+    private List<Word> words;
+
+    public void add(Word word) {
+	words.add(word);
+    }
+
+    public Sentence() {
+	this.words = new ArrayList<Word>();
+    }
+
+    public List<Word> getWords() {
+	return words;
+    }
+
+    public void setWords(List<Word> sentence) {
+	this.words = sentence;
+    }
+
+    @Override
+    public String toString() {
+	return "Words: " + words;
+    }
+
+}
---
+++ task1/Task1.java
@@ -0,0 +1,39 @@
+package by.htp.hw4.task1;
+/*
+ * 1. Создать объект класса Текст, используя классы Предложение, Слово. 
+ * Методы: дополнить текст, вывести на консоль текст, заголовок текста.
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+	Sentence sentence = new Sentence();
+	sentence.add(new Word("Hello"));
+	sentence.add(new Word("Java"));
+	sentence.add(new Word("World!"));
+
+	Text text = new Text();
+	text.append(sentence);
+
+	Sentence headText = new Sentence();
+	headText.add(new Word("Say hello!"));
+	text.setHead(headText);
+
+	printText(text);
+    }
+
+    private static void printText(Text text) {
+	for (Word word : text.getHead().getWords()) {
+	    System.out.print(word.getWord() + " ");
+	}
+
+	System.out.println();
+	for (Sentence sentence : text.getText()) {
+	    for (Word word : sentence.getWords()) {
+		System.out.print(word.getWord() + " ");
+	    }
+	}
+    }
+}
+
+}
